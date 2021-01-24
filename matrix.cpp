73b62e46a905082ef5be2ab40aa1d0644cd22f43
Thomas288 <t.j.koopman@students.uu.nl>
2021-01-24 16:01:20 +0100

    Update matrix.cpp
    Poging 2

--- matrix.cpp
+++ matrix.cpp
@@ -1,134 +1,134 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
+template<typename T>
 class Matrix {
 public:
-    int rows;
-    int columns;
-    int* contents;
+
+    // Default constructor
+    Matrix();
 
     // Constructor
-    // data is the contents of the matrix in a list
-    // like you read a book
-    Matrix(int m, int n, int* data) {
-        rows = m;
-        columns = n;
-        
-        // We copy data to a dynamically allocated piece of memory
-        // to make the program more robust.
-        contents = new int[m*n];
-        
-        for (int i = 0; i < m*n; i++) {
-            contents[i] = data[i];    
+    Matrix(int m, int n, std::vector<T> data): rows_{m}, columns_{n} {
+        if (m*n != (int) data.size()) {
+            throw "Dimensions do not match data!";
         }
-    }       
-    
-    // Returns the element in row i, column j
-    int getElement(int i, int j) {
-        return contents[columns*i + j];
+        data_ = data;
     }
 
-    // Gets the i'th row of this matrix
-    int* getRow(int i) {
-        // Dynamic because we need the row outside this helper function
-        int* row = new int[columns];
-        
-        for (int j = 0; j < columns; j++) {
-            row[j] = getElement(i, j);
-        }
-        
-        return row;
+    // Copy constructor (makes a copy of the argument)
+    Matrix(const Matrix<T>& other): rows_{other.rows_}, columns_{other.columns_} {
+        data_ = other.data_;
     }
-    
-    // Gets a the j'th column of matrix other
-    int* getColumn(int j) {
-        // Dynamic because we need the column outside this helper function
-        int* column = new int[rows];
-        
-        for (int i = 0; i < rows; i++) {
-            column[i] = getElement(i, j);
-        }
-        
-        return column;
+
+    // Move constructor (moves the information of the argument to the created object, the
+    // argument is destroyed)
+    Matrix(Matrix<T>&& other): rows_{other.rows_}, columns_{other.columns_}, data_{other.data_} {
+        other.data_.~vector();
     }
-    
-    Matrix addition(Matrix other) {
-        int* sum = new int[rows*columns];
-        
-        for (int i = 0; i < rows*columns; i++) {
-            sum[i] = contents[i] + other.contents[i];    
+
+    // Copy assignment operator (overloads = when we have an object on the other side of =)
+    Matrix<T>& operator=(const Matrix<T>& other) {
+        if (this != &other) {
+            rows_ = other.rows_;
+            columns_ = other.columns_;
+            data_ = other.data_;
         }
-        
-        Matrix sumMatrix(rows, columns, sum);
-        
-        delete sum;
-        
-        return sumMatrix;
+
+        return *this;
     }
-    
-    Matrix product(Matrix other) {
-        int* product = new int[rows*other.columns];
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < other.columns; j++) {
-                int* row = getRow(i);
-                int* column = other.getColumn(j);
-                int value = innerProduct(row, column, columns); // Our row and the other guy's column contain as many elements as we have columns
-                                
-                delete row;
-                delete column;
-                product[other.columns*i + j] = value;
-            }
+
+    // Move assignment operator (overloads = when we have a reference on the other side =)
+    Matrix<T>& operator=(Matrix<T>&& other) {
+        if (this != &other) {
+            rows_ = other.rows_;
+            columns_ = other.columns_;
+            data_ = &other.data_;
+            other.data_.~vector();
         }
-        
-        Matrix productMatrix(rows, other.columns, product);
-        
-        delete product;
-        
-        return productMatrix;
+
+        return *this;
     }
-    
-    void print() {
-                
-        for (int i = 0; i < rows; i++) {
-            std::cout << '{';
-            for (int j = 0; j < columns; j++) {
-                std::cout << getElement(i, j) << ", ";
-            }
-            std::cout << "}\n";
+
+    // Destructor
+    ~Matrix() {
+        data_.~vector();
+    }
+
+    // Returns a reference to the entry at (i, j), so we can change it.
+    T& operator()(int i, int j) {
+        return data_.at(columns_*i + j);
+    }
+
+    Matrix<T> operator+(const Matrix<T>& other) {
+        if (columns_ != other.columns_ || rows_ != other.rows_) {
+            throw "Addition failure: dimensions do not match";
         }
-        
-    }
-    
-private:    
-    // Computes the inner product between
-    // vec1, vec2, both vectors of length.
-    int innerProduct(int* vec1, int* vec2, int length) {
-        int sum = 0;
-        for (int i = 0; i < length; i++) {
-            sum += vec1[i]*vec2[i];
-        }        
+
+        // (Why doesn't 'Matrix<T> sum = this' work as an application of the copy constructor?)
+        Matrix<T> sum(rows_, columns_, data_);
+        for (int i = 0; i < rows_*columns_; i++) {
+            sum.data_[i] += other.data_[i];
+        }
+
         return sum;
     }
-    
+
+    // Design question: how can I change operator() to allow other to be constant?
+    Matrix<T> operator*(Matrix<T>& other) {
+        if (columns_ != other.rows_) {
+            throw "Multiplication error: dimensions do not match";
+        }
+
+        std::vector<T> product;
+
+        for (int i = 0; i < rows_; i++) {
+            for (int j = 0; j < other.columns_; j++) {
+                T sum = 0;
+                for (int k = 0; k < columns_; k++) {
+                    sum += other(k, j) * this->operator()(i, k);
+                }
+                product.push_back(sum);
+            }
+        }
+
+        return Matrix(rows_, columns_, product);
+    }
+
+private:
+    int rows_;
+    int columns_;
+    std::vector<T> data_;
+
+    template<typename U>
+    friend std::ostream& operator<<(std::ostream& outputStream, Matrix<U>& matrix);
 };
 
+template<typename U>
+std::ostream& operator<<(std::ostream& outputStream, Matrix<U>& matrix) {
+
+    for (int i = 0; i < matrix.rows_; i++) {
+        outputStream << '{';
+        for (int j = 0; j < matrix.columns_ - 1; j++) {
+            outputStream << matrix(i, j) << ", ";
+        }
+        outputStream << matrix(i, matrix.columns_ - 1) << "}\n";
+    }
+
+    return outputStream;
+}
+
 int main() {
-    int data1[6] = {0, 1, 
-                    1, 0,
-                    2, 2};
-    Matrix mat1(3, 2, data1);
-    
-    int data2[8] = {1, 2, 3, 4,
-                    5, 6, 7, 8};
-    Matrix mat2(2, 4, data2);
-    
-    Matrix product = mat1.product(mat2);
-    
-    std::cout << "Matrix 1 looks like\n";
-    mat1.print();
-    std::cout << "Matrix 2 looks like\n";
-    mat2.print();
-    std::cout << "The product looks like \n";
-    product.print();
+    std::vector<int> data{1, 2, 3, 4};
+    Matrix<int> mat(2, 2, data);
+
+    Matrix<int> sum = mat + mat;
+    Matrix<int> product = mat * mat;
+
+    std::cout << mat << "+\n" << mat << "=\n" << sum;
+    std::cout << "------------------" << std::endl;
+    std::cout << mat << "*\n" << mat << "=\n" << product;
+
+    mat(0, 0) = 0;
+    std::cout << "Changed mat:\n" << mat;
 }
